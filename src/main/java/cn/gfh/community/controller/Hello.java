ef961c480df12cc126703efb01720bbb6aae70a2
karolguo7 <karolguo7@gmail.com>
2019-10-12 14:23:12 +0800

    init community

---
+++ src/main/java/cn/gfh/community/controller/Hello.java
@@ -0,0 +1,21 @@
+package cn.gfh.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @author Karol
+ * @project_name community_demo
+ * @create_date 2019-10-11 17:08
+ */
+@Controller
+public class Hello {
+
+    @RequestMapping("/hello")
+    public String toIndex(@RequestParam(name = "name") String name, Model model){
+        model.addAttribute("name",name);
+        return "index";
+    }
+}
